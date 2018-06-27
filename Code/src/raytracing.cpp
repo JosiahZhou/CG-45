68779a8b33b02e31f347f7e603d5e280c708c78c
Daniel van den Akker <d.a.vandenakker@student.tudelft.nl>
2018-06-27 14:17:29 +0200

    show boxes

--- Code/src/raytracing.cpp
+++ Code/src/raytracing.cpp
@@ -634,10 +634,7 @@ void yourDebugDraw()
 	glPopAttrib();
 
 	// draw the bounding boxes
-	//for (Box box : root->getBoxes())
-	//{
-	//	box.highlightEdges();
-	//}
+	root->highlightEdges();
 
 	for (int i = 0; i < 20; i++) {
 		drawRay(recurseTestRayOrigins[i], recurseTestRayDestinations[i], Vec3Df(0, 1, 0));
